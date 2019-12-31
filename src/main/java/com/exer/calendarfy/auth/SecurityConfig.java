74ba75f10a35840443ce708bb55f8b98c8b9224b
exerdra <exerdra@gmail.com>
2019-12-30 21:39:34 -0600

    added basic auth

---
+++ src/main/java/com/exer/calendarfy/auth/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.exer.calendarfy.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
+        .authorizeRequests().anyRequest().authenticated()
+        .and()
+        .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("acbd885d-318f-4aa0-93ec-82946f681dc9")
+                .password("{noop}6275c988-c99f-4e99-9db1-3340907bebe5")
+                .roles("USER");
+    }
+
+}
